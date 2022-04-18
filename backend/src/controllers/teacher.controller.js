5b30c1f267a642ca34580a37ac774436f34545d6
chandudeore <chandudeore2016@gmail.com>
2022-04-18 14:08:26 +0530

    modify

--- backend/src/controllers/teacher.controller.js
+++ backend/src/controllers/teacher.controller.js
@@ -16,12 +16,30 @@ router.post("", async (req, res) => {
 
 router.get("", async (req, res) => {
   try {
-    const page = req.body.page || 1;
-    const size = req.body.size;
+    const page = req.query.page;
+
     const teacher = await Teacher.find()
+      .skip((page - 1) * 5)
+      .limit(5)
       .populate({ path: "class_id", select: ["className", "subject"] })
       .lean()
       .exec();
+    console.log(req.query.srt);
+    //let res = req.query.srt;
+    //console.log(res);
+    // if (res === "asc") {
+    //   teacher.sort((a, b) => {
+    //     return a.age - b.age;
+    //   });
+    //   return res.send(teacher);
+    // }
+    // if (res === "dsc") {
+    //   console.log(res);
+    //   teacher.sort((a, b) => {
+    //     return b.age - a.age;
+    //   });
+    //   return res.send(teacher);
+    // }
 
     return res.status(202).send(teacher);
   } catch (err) {
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
-import { logOut } from "../Redux/adminlogin/action";
-import { Details } from "./Details";
+import { Navigate } from "react-router";
 
 export const Home = () => {
-  //const { isAuthenticated } = useSelector((state) => state.login);
+  const { isAuthenticated } = useSelector((state) => state.login);
 
-  //const dispatch = useDispatch();
-  const [data, setData] = useState();
+  const dispatch = useDispatch();
+  const [data1, setData] = useState([]);
+  const [sorting, setSorting] = useState("");
+  const [page, setPage] = useState(1);
 
   const getData = () => {
-    fetch("http://localhost:8080/teacher")
+    fetch(`http://localhost:8080/teacher?page=${page}`)
       .then((res) => res.json())
       .then((res) => setData(res))
       .catch((err) => console.log("Error"));
   };
   useEffect(() => {
     getData();
-  }, []);
-
-  // if (!isAuthenticated) {
-  //   return <Navigate to="//admin-login"></Navigate>;
-  // }
+  }, [page]);
+  //console.log(page);
+  if (!isAuthenticated) {
+    return <Navigate to="/admin-login"></Navigate>;
+  }
 
   // const handleLogout = () => {
   //   dispatch(logOut);
@@ -30,16 +30,39 @@ export const Home = () => {
 
   return (
     <>
-      <div></div>
-      {data.map((e) => (
-        <Details
-          key={e.id}
-          name={e.name}
-          age={e.age}
-          gender={e.gender}
-          clss={e.class_id.length}
-        ></Details>
-      ))}
+      <div>
+        <button onClick={() => setPage(1)}>1</button>
+        <button onClick={() => setPage(2)}>2</button>
+        <button onClick={() => setPage(3)}>3</button>
+        <button onClick={() => setPage(4)}>4</button>
+        <button onClick={() => setPage(5)}>5</button>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>No-Of-Classes</th>
+          </thead>
+          <tbody>
+            {data1.map((e) => {
+              return (
+                <tr key={e.id}>
+                  <td>{e.name}</td>
+                  <td>{e.age}</td>
+                  <td>{e.gender}</td>
+                  <td>{e.class_id.length}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button onClick={() => setSorting("asc")}>lot to High</button>
+        <button onClick={() => setSorting("dsc")}>High to low</button>
+      </div>
     </>
   );
 };
